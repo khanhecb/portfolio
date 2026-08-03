05bc867de9859687901c1097e4133980db4d9baf
Nguyễn Hữu Khánh <nguyenhuukhanhwork@gmail.com>
2026-08-03 22:26:46 +0700

    Refactor
    Use JSON and HTML render layout Project home page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,98 +1,74 @@
-/** 
- * TÊN FILE: main.js
- * CHỨC NĂNG: Load Component HTML, Quản lý Dữ liệu và Render Giao diện
- */
+// Render Layout Projects
+async function renderProjects(dataFile = './data/projects.json', containerId = 'projects-records') {
+    try {
 
-// ==========================================
-// PHẦN 1: DỮ LIỆU (DATA)
-// ==========================================
-const projectsData = [
-    {
-        year: "2025 - Now",
-        name: "Bảo trì",
-        url: "",
-        tags: ["Cpanel", "SSH", "Maintain", "Migrate", "Deploy", "Backup", "Error", "Web Mail", "DNS"]
-    },
-    {
-        year: "2026",
-        name: "GuangNa Furniture",
-        url: "https://guangnafurniture.com",
-        tags: ["Flatsome", "Woocommerce", "Cloudflare", "HTML & CSS", "PHP + ACF"]
-    },
-    {
-        year: "2026",
-        name: "CPAP - Bpharm",
-        url: "https://cpap.com.vn",
-        tags: ["Flatsome", "Woocommerce", "Cloudflare", "HTML & CSS & JS", "Redis"]
-    },
-    {
-        year: "2026",
-        name: "Trutech",
-        url: "https://trutech-mfg.com.vn/",
-        tags: ["Ainexa", "Elementor", "HTML/CSS/JS", "Light/Dark Mode"]
-    },
-    {
-        year: "2026",
-        name: "Danh Ca Minh Cảnh",
-        url: "https://danhcaminhcanh.com",
-        tags: ["Gutenberg", "Woocommerce", "Python", "GG Sheet", "Telegram", "Sepay"]
-    },
-    {
-        year: "2026",
-        name: "Nguyễn Hải Media",
-        url: "https://nguyenhaimedia.com",
-        tags: ["FSE", "Gutenberg", "ACF"]
-    },
-    {
-        year: "2025",
-        name: "Cricket Fashion",
-        url: "https://cricketfashion.vn/",
-        tags: ["Elementor", "Theme The7"]
-    },
-    {
-        year: "2026",
-        name: "LandingPage mua vé",
-        url: "https://intwomencelebration.org/ticker-2026/",
-        tags: ["Paypal", "Forminator"]
-    },
-    {
-        year: "2025",
-        name: "Form đăng ký khám bệnh",
-        url: "https://sleepfi.vn/",
-        tags: ["Forminator", "GG Sheet Script", "Email SMTP", "PHP"]
+        // Get JSON data
+        const response = await fetch(dataFile);
+
+        if (!response.ok) throw new Error(`HTTP Error: ${response.status}`);
+
+        const projectsData = await response.json();
+
+        // Check Container existed
+        const tbody = document.getElementById(containerId);
+
+        if (!tbody) {
+            console.warn(`Not Found #${containerId} in DOM`);
+            return;
+        }
+
+        // Render HTML
+        tbody.innerHTML = projectsData.map(({ name, url, year, tags }) => {
+            const hasUrl = Boolean(url);
+
+            const nameColumn = hasUrl 
+                ? `<td><a class="text-color-primary" href="${url}" target="_blank" rel="noopener">${name}</a></td>`
+                : `<td class="text-color-primary">${name}</td>`;
+
+            const linkColumn = hasUrl 
+                ? `<td class="hide-on-mobile"><a href="${url}" target="_blank" rel="noopener">${url}</a></td>`
+                : `<td class="hide-on-mobile"></td>`;
+
+            const tagsHTML = Array.isArray(tags) 
+                ? tags.map(tag => `<span class="tag-item">${tag}</span>`).join('')
+                : '';
+
+            return `
+                <tr>
+                    <td class="text-color-second">${year}</td>
+                    ${nameColumn}
+                    ${linkColumn}
+                    <td class="tags flex flex-wrap hide-on-mobile">${tagsHTML}</td>
+                </tr>
+            `;
+        }).join('');
+
+    } catch (error) {
+        console.error("Error: Error when render HTML Projects Layout: ", error);
     }
-];
+}
 
+async function renderLayoutSkills(dataFile = './data/skills.json', containerId = 'skills') {
 
+    try {
+        const response = await fetch(dataFile);
+        
+        if (!response.ok) {
+            throw new Error(`HTTP Error: ${response.status}`);
+        }
 
-// Load 
-function renderProjects() {
-    const tbody = document.getElementById('projects-tbody');
-    if (!tbody) {
-        console.warn("Chưa tìm thấy #projects-tbody trong DOM");
-        return;
+        const skillsJson = await response.json();
+
+        // Check container DOM
+        const container = document.getElementById(containerId);
+        if (!container) {
+            console.warn(`DOM ID not found #${containerId}`);
+            return;
+        }
+
+    } catch (error){
+        console.error("File not found", error);
     }
-
-    tbody.innerHTML = projectsData.map(project => {
-        const nameColumn = project.url 
-            ? `<td><a class="text-color-primary" href="${project.url}" target="_blank" rel="noopener">${project.name}</a></td>`
-            : `<td class="text-color-primary">${project.name}</td>`;
-
-        const linkColumn = project.url 
-            ? `<td class="hide-on-mobile"><a href="${project.url}" target="_blank" rel="noopener">${project.url}</a></td>`
-            : `<td class="hide-on-mobile"></td>`;
-
-        const tagsHTML = project.tags.map(tag => `<span class="tag-item">${tag}</span>`).join('');
-
-        return `
-            <tr>
-                <td class="text-color-second">${project.year}</td>
-                ${nameColumn}
-                ${linkColumn}
-                <td class="tags flex flex-wrap hide-on-mobile">${tagsHTML}</td>
-            </tr>
-        `;
-    }).join('');
 }
 
 // Load HTML Component
@@ -114,6 +90,8 @@ async function loadIncludes() {
 async function initApp() {
     await loadIncludes();
     renderProjects();
+    renderLayoutSkills();
 }
 
 document.addEventListener('DOMContentLoaded', initApp);
+
